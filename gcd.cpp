d771be2e1ddec8eea6d65c3d16efc85564c6a880
Hari Om Yadav <66957239+hariom1625@users.noreply.github.com>
2021-03-20 22:39:44 +0530

    add gcd

---
+++ gcd.cpp
@@ -0,0 +1,27 @@
+int gcd(int a, int b)
+{
+  
+    if (a == 0)
+       return b;
+    if (b == 0)
+       return a;
+  
+   
+    if (a == b)
+        return a;
+  
+    
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
+}
+
+
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b); 
+     
+}
